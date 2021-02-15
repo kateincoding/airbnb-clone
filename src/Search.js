26bc78f15bb2bc1797ef73487f190d70e341e4aa
Your Name <k.soto@pucp.pe>
2021-02-14 22:34:04 -0500

    installing npm i react-date-range

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Search.css'
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; //theme css file
+import { DateRangePicker } from "react-date-range";
 
+//Date Picker component
 function Search() {
     return (
         <div className= 'search'>
